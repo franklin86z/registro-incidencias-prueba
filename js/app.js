bb425b618045efbdf978ac5f6fd8d8ba26f36513
franklin86z <33067341+franklin86z@users.noreply.github.com>
2026-07-01 11:38:27 -0500

    Add guardar function to collect form data

---
+++ js/app.js
@@ -0,0 +1,23 @@
+function guardar(){
+
+let datos={
+
+incidencia:document.getElementById("incidencia").value,
+
+nodo:document.getElementById("nodo").value,
+
+olt:document.getElementById("olt").value,
+
+responsable:document.getElementById("responsable").value,
+
+fecha:document.getElementById("fecha").value,
+
+hora:document.getElementById("hora").value
+
+};
+
+console.log(datos);
+
+alert("Registro listo para enviar");
+
+}
